3b5cab7124e687fa9d27a84405569d92276f6fd7
Матвей Рыбин <rybin.mat32@gmail.com>
2025-05-24 22:46:20 +0300

    date format changing function enabled

--- src/widget.py
+++ src/widget.py
@@ -31,3 +31,5 @@ print(mask_account_card("Visa Classic 6831982476737658"))
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+def get_date()
