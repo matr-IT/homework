fdbf27da3e2cc41c397a9bcfafedb94d2ac0817e
Матвей Рыбин <rybin.mat32@gmail.com>
2025-06-01 19:15:09 +0300

    filter by state function init

---
+++ src/processing.py
@@ -0,0 +1,15 @@
+
+our_list = [
+{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+def filter_by_state(list_of_dicts: list[dict], key="EXECUTED") -> list[dict]:
+    """sorting list by state"""
+    new_list = []
+    for i in list_of_dicts:
+        if i["state"] == key:
+            new_list.append(i)
+    return new_list
