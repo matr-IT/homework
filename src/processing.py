f7e2b2128833b4640d315b49645dc228c605fe25
Матвей Рыбин <rybin.mat32@gmail.com>
2025-06-03 23:10:47 +0300

    sort by date function fix

--- src/processing.py
+++ src/processing.py
@@ -7,7 +7,10 @@ def filter_by_state(list_of_dicts: list[dict], key: str = "EXECUTED") -> list[di
     return new_list
 
 
-def sort_by_date(list_of_dicts: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(list_of_dicts: list[dict], ascending: bool = True) -> list[dict]:
     """sorting by date"""
-    list_of_dicts.sort(key=lambda x: x["date"])
+    if ascending:
+        list_of_dicts.sort(key=lambda x: x["date"])
+    if not ascending:
+        list_of_dicts.sort(key=lambda x: x["date"], reverse=True)
     return list_of_dicts
