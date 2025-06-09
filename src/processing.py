fdb1eff7dfb5db4361da2cf03cdc169b1ea8adf2
rybin <rybin.32@gmail.com>
2025-06-09 22:59:02 +0300

    tests for function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,17 @@
-def filter_by_state(list_of_dicts: list[dict], key: str = "EXECUTED") -> list[dict]:
+from typing import List
+
+# qwer = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'EXECUTED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'EXECUTED', 'date': '2018-10-14T08:21:33.419441'}]
+
+def filter_by_state(list_of_dicts: list[dict], key: str = "EXECUTED") -> str | list[dict]:
     """sorting list by state"""
     new_list = []
     for i in list_of_dicts:
         if i["state"] == key:
             new_list.append(i)
-    return new_list
+    if len(new_list) != 0:
+        return new_list
+    else:
+        return "Нет операций по данному ключу"
 
 
 def sort_by_date(list_of_dicts: list[dict], descending: bool = True) -> list[dict]:
--- tests/conftest.py
+++ tests/conftest.py
@@ -93,7 +93,6 @@ def start_format_date_2():
 @pytest.fixture
 def empty_date():
     return ""
-# Фикстурты для модуля processing.py
 
 @pytest.fixture
 def non_standard_date_1():
@@ -102,3 +101,12 @@ def non_standard_date_1():
 @pytest.fixture
 def non_standard_date_2():
     return "2023-10-05T12:30:45+03:00"
+
+# Фикстурты для модуля processing.py
+@pytest.fixture
+def list_of_dicts_1():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+@pytest.fixture
+def list_of_dicts_2():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
