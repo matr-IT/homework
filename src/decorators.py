14b4240a14aedd5de67268e9e5d354fd8f0e32b2
rybin <rybin.32@gmail.com>
2025-06-22 21:44:29 +0300

    log decorator init

---
+++ src/decorators.py
@@ -0,0 +1,37 @@
+from functools import wraps
+
+
+def log(filename=None):
+    """decorator fo function logging"""
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            try:
+                result = func(*args, **kwargs)
+                success_message = f"{func} ok"
+
+                if filename:
+                    with open(filename, "a") as f:
+                        f.write(success_message + "\n")
+                else:
+                    print(success_message)
+
+                return result
+
+            except Exception as e:
+                error_message = f"{func} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+
+                if filename:
+                    with open(filename, "a") as f:
+                        f.write(error_message + "\n")
+                else:
+                    print(error_message)
+
+                raise
+
+        return wrapper
+
+    return decorator
+
