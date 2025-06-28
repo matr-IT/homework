e0b493e469694e616d4677c5f968564ebc096391
rybin <rybin.32@gmail.com>
2025-06-28 17:20:02 +0300

    small fixes

--- src/external_api.py
+++ src/external_api.py
@@ -19,3 +19,9 @@ def transaction_amount(transaction):
 
             response = requests.get(url, headers=headers)
             return response.json()["result"]
+
+print(transaction_amount({"operationAmount": {
+      "amount": "71771.90",
+      "currency": {
+        "name": "USD",
+        "code": "USD"}}}))
---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+from unittest.mock import Mock
+from src.utils import open_json
+
+def test_open_json():
+    pass
