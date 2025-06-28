11fe679454d7cc864d037f6393e284b6d071ded7
rybin <rybin.32@gmail.com>
2025-06-28 15:13:30 +0300

    function open_json int

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import json
+from json import JSONDecodeError
+
+
+def open_json (path: str) -> list:
+    with open(path, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except JSONDecodeError:
+            return []
+        except Exception:
+            return []
+
