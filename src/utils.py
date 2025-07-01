d01b251006a219ac346a8d88ab7c1fb3ab3640a5
rybin <rybin.32@gmail.com>
2025-07-01 21:19:46 +0300

    loggers for masks.py and utils.py init

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,37 @@
 import json
+import logging
 from json import JSONDecodeError
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # Перейти на 2 уровня вверх из src/utils.py
+LOG_DIR = BASE_DIR / "logs"  # Директория логов в корне проекта
+LOG_FILE = LOG_DIR / "utils.log"
+
+LOG_DIR.mkdir(exist_ok=True, parents=True)
+
+logging.basicConfig(filemode='w')
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 
 def open_json(path: str) -> list:
     """function converts json-files to python list of dicts"""
     with open(path, "r", encoding="utf-8") as f:
         try:
+            logger.info(f'Открываем JSON-файл по ссылке {path}')
             return json.load(f)
-        except FileNotFoundError:
+        except FileNotFoundError as ex:
+            logger.error(f'Произошла ошибка: {ex}. Файл {path} не найден')
             return []
-        except JSONDecodeError:
+        except JSONDecodeError as ex:
+            logger.error(f'Произошла ошибка: {ex}. Ошибка декодирования файла {path}')
             return []
-        except Exception:
+        except Exception as ex:
+            logger.error(f'Произошла ошибка: {ex}')
             return []
 
-print(open_json("/Users/rybin/PycharmProjects/homework/data/operations.json"))
