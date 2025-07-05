dc957dfdbbbf19128d72013da34bbbe22cb4bf77
rybin <rybin.32@gmail.com>
2025-07-05 14:49:25 +0300

    red_csv func fixes

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 def read_csv(csv_path: str) -> list[dict]:
     df = pd.read_csv(csv_path)
-    return df.to_dict(orient="records", delimeter=";")
+    return df.to_dict(orient="records")
+
+if __name__ == '__main__':
+    print(read_csv('/Users/rybin/PycharmProjects/homework/data/transactions.csv'))
