8bb6fbfdedf15c66cc1f769af60dffb56510457a
rybin <rybin.32@gmail.com>
2025-07-05 14:53:47 +0300

    red_excel func init

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 def read_csv(csv_path: str) -> list[dict]:
+    """Function reads csv-files and returns list of dicts"""
     df = pd.read_csv(csv_path)
     return df.to_dict(orient="records")
 
-if __name__ == '__main__':
-    print(read_csv('/Users/rybin/PycharmProjects/homework/data/transactions.csv'))
+
+def read_excel(csv_path: str) -> list[dict]:
+    """Function reads excel-files and returns list of dicts"""
+    df = pd.read_excel(csv_path)
+    return df.to_dict(orient="records")
+
+if __name__ == "__main__":
+    print(read_excel('/Users/rybin/PycharmProjects/homework/data/transactions_excel.xlsx'))
