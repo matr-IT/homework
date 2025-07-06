ec8bb5d38b2474959cf317f2bbba4ba329d1e3f5
rybin <rybin.32@gmail.com>
2025-07-06 21:17:44 +0300

    tests for reed_csv_excel init

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -11,7 +11,3 @@ def read_excel(csv_path: str) -> list[dict]:
     """Function reads excel-files and returns list of dicts"""
     df = pd.read_excel(csv_path)
     return df.to_dict(orient="records")
-
-
-if __name__ == "__main__":
-    print(read_excel("/Users/rybin/PycharmProjects/homework/data/transactions_excel.xlsx"))
